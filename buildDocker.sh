f73ef55dc42d52005a13c1fc2e0957285be09919
deB4SH <github@b4sh.de>
2025-03-17 21:05:00 +0100

    fix: build cli call

--- buildDocker.sh
+++ buildDocker.sh
@@ -29,5 +29,13 @@ then
 else
     TAG=$GIT_TAG
 fi
-# build yart
-docker build -f Dockerfile -t $(echo "$REGISTRY/bot:$TAG")
+# build
+if [ -x "$(command -v podman)" ]; then
+    cli_cmd="podman"
+elif [ -x "$(command -v docker)" ]; then
+    cli_cmd="docker"
+else
+    echo "No container cli tool found! Aborting."
+    exit -1
+fi
+${cli_cmd} build -f Dockerfile -t $(echo "$REGISTRY/bot:$TAG")
