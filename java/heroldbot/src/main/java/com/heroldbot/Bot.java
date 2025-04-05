413089ad6a92a1ee41a44a90b092fe9df07b2fc3
Phil Schedemann <phil.schedemann@web.de>
2025-04-05 21:34:03 +0200

    inital java push

---
+++ java/heroldbot/src/main/java/com/heroldbot/Bot.java
@@ -0,0 +1,27 @@
+package com.heroldbot;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import javax.security.auth.login.LoginException;
+
+public class Bot extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException {
+        // Token aus der Umgebungsvariable holen
+        String token = System.getenv("TOKEN");
+        
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.setActivity(Activity.playing("Tournament Management"));
+        builder.addEventListeners(new Bot());
+        builder.build();
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("test_log")) {
+            event.reply("✅ Logger works!").queue();
+        }
+        // Weitere Slash-Commands hier hinzufügen
+    }
