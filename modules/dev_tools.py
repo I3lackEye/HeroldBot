bcb00b7abe4e53e9738accaa7c815bb4c18c2a81
I3lackEye <phil.schedemann@web.de>
2026-02-06 23:43:16 +0100

    Fixed: Missing add_task import in dev_tools.py
    The test_reschedule command used add_task but it wasn't imported,
    causing NameError when trying to create reschedule timers.

--- modules/dev_tools.py
+++ modules/dev_tools.py
@@ -21,7 +21,7 @@ from modules.dataStorage import (
 )
 from modules.embeds import build_embed_from_template, load_embed_template
 from modules.logger import logger
-from modules.task_manager import get_all_tasks
+from modules.task_manager import add_task, get_all_tasks
 from modules.tournament import auto_end_poll, close_registration_after_delay, end_tournament_procedure
 from modules.utils import (
     generate_random_availability,
